dfab91aabe1870b162d558d8d9cb6b6d22b4c903
caroline lacerda <lacerdaccarol@gmail.com>
2022-11-29 17:22:21 -0300

    landing page created

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :drivers
   devise_for :admins, controllers: { registrations: "admins/registrations", sessions: "admins/sessions",
                                      passwords: "admins/passwords" }
-  root to: "pages#home"
+  root to: "pages#landing"
+  get "home", to: "pages#home"
 end
