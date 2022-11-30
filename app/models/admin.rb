30045af1cc77d47e25850fbf33bfec68a8032267
Pedro Padilha <pedrofpadilha@gmail.com>
2022-11-30 18:08:58 -0300

    show de driver e navbar

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,6 +5,7 @@ class Admin < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :user
   after_commit :create_parent
+
   def create_parent
     User.create(phone_number: self.user_phone_number, admin: self, first_name: self.user_first_name, last_name: self.user_last_name)
   end
